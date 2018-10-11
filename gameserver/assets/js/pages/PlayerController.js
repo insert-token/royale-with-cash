5ade6cd45925b01a0005a55870bd4a607ac1975c
Matt M <matt@puroxy.org>
2018-10-11 09:05:13 -0700

    ready for new player attrs

--- gameserver/assets/js/pages/PlayerController.js
+++ gameserver/assets/js/pages/PlayerController.js
@@ -2,13 +2,14 @@ let vm = new Vue({
   el: '#home-wrap',
   data: {
     players: [],
+    kills: 0,
     user: {
       name: ''
     },
     updateIntervalId: undefined
   },
   beforeMount: function() {
-
+      
   },
   mounted: function() {
 
@@ -17,29 +18,21 @@ let vm = new Vue({
     }, 1000);
 
     io.socket.on('inputName', () => {
-
       var playerName = prompt('Please enter your name.') || 'Greg Maxwell';
-
       io.socket.post('/setPlayerName', { name: playerName }, (serverResponse) => {
-
         this.user = serverResponse;
         io.socket.post('/getMap', { mapId: g.mapId });
       });
-
     });
 
     // Tell the socket we're here and fetch any session
     // data that has been stored for the user.
     io.socket.get('/check-in', (userObject) => {
-
       if (userObject.name) {
-
         this.user = userObject;
         io.socket.post('/getMap', { mapId: g.mapId });
       }
     });
-
-
   },
   methods: {
 
